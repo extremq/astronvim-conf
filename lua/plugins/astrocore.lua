a5d33da9a3c8f98b741094ab495cb6900b46476e
extremq <45830561+extremq@users.noreply.github.com>
2024-09-14 16:22:59 +0300

    update stuff

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -65,6 +65,9 @@ return {
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
       },
+      t = {
+        ["<Esc><Esc>"] = { "<C-\\><C-n>", desc = "Enter Normal Mode in Terminal" },
+      },
     },
   },
 }
