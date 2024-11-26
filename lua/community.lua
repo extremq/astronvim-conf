b5509129477fc569012d8f9023cf2fe7a961b81b
extremq <contact@extremq.com>
2024-11-26 18:46:02 +0200

    update
    lets go

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.recipes.vscode-icons" },
 }
